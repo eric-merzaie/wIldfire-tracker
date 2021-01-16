abaa38e42667c1a2ae62db6cdd1eac95d5ebef15
My Name <eric.merzaie@gmail.com>
2021-01-16 19:02:49 +1100

    added location info box

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,15 @@
+import { useState } from 'react'
 import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
+import LocationInfoBox from './LocationInfoBox'
 require ("dotenv").config();
 
 const Map = ({ eventData, center, zoom, api_key }) => {
+    const [LocationInfo, setLocationInfo] = useState(null)
+
     const markers = eventData.map((ev, index) => {
         if(ev.categories[0].id === 8) {
-            return <LocationMarker key={index} lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} />
+            return <LocationMarker key={index} lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
         }
         return null
     })
@@ -19,6 +23,7 @@ const Map = ({ eventData, center, zoom, api_key }) => {
             >
                 {markers}
             </GoogleMapReact>
+            {LocationInfo && <LocationInfoBox info={LocationInfo} />}
         </div>
     )
 }
